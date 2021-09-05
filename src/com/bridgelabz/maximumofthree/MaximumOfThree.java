d2e728d8a22a1ffc4eb140a311c67b873e22b5cc
srinadh1304 <srinadh1304@gmail.com>
2021-09-05 07:44:32 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/maximumofthree/MaximumOfThree.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.maximumofthree;
+
+public class MaximumOfThree {
+public static void main(String[] args) {
+	System.out.println("Welcome to maximum of three computation program");
+}
+}
